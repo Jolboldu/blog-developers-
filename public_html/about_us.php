f711f3ef239e65ae9fed5429226eb7b6b925df24
Jolboldu <44244228+Jolboldu@users.noreply.github.com>
2018-12-17 11:41:59 +0600

    Add files via upload

---
+++ public_html/about_us.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+require_once 'styles.php';
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 25%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+</style>
+</head>
+<body>
+
+<h2>Developers-Team</h2>
+<p>This site helps people to share with information and can be used by AUCA students as microblog. This is an online publishing platform developed by 'Developers' team, and launched in Deceber 2018. The platform is an example of social journalism, having a hybrid collection of amateur and professional people and publications.</p>
+<br>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+
+      <div class="container">
+        <h2>Amantur</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+
+      <div class="container">
+        <h2>Beksultan</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+
+      <div class="container">
+        <h2>Jolboldu</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+	<div class="column">
+    <div class="card">
+
+      <div class="container">
+        <h2>Temirbek</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
